c3b2b643187b547ede0cf6276b711dda86e4660b
crummm <toddmz+github@gmail.com>
2018-05-15 05:52:47 -0500

    added modules example with no typings (for comparison)

---
+++ examples/modules-example-no-typings/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <h1>Title: {{ title }}</h1>
+    <button @click="decrementAction">-3 (action)</button>
+    counter: {{ count }}
+    <button @click="incrementMutation">+3 (mutation)</button>
+    <p>
+      counter (x10): {{ countX10 }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import { mapState, mapMutations, mapActions, mapGetters } from 'vuex';
+
+  export default Vue.extend({
+    name: 'app',
+    computed: {
+      ...mapState({
+        title: 'title',
+        count(state) { return state.CounterStore.count; },
+      }),
+      ...mapGetters('CounterStore', [
+        'countX10',
+      ])
+    },
+    methods: {
+      ...mapMutations('CounterStore', {
+        incrementMutation(commit): void {
+          // @ts-ignore
+          commit('increment', 3);
+        },
+      }),
+      ...mapActions({
+        decrementAction(dispatch): void {
+          // @ts-ignore
+          dispatch('CounterStore/decrement', 3);
+        },
+      }),
+    },
+  });
+</script>
+
+<style>
+  .app {
+    margin-top: 30px;
+  }
+  button {
+    background-color: dodgerblue;
+    border: none;
+    border-radius: 5px;
+    color: white;
+    margin: 4px;
+    padding: 8px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+  }
+</style>
---
+++ examples/modules-example-no-typings/src/store.ts
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    title: 'Module Example',
+  },
+  modules: {
+    CounterStore: {
+      namespaced: true,
+      state: {
+        count: 0,
+      },
+      mutations: {
+        decrement(state, payload) {
+          state.count -= payload;
+        },
+        increment(state, payload: number) {
+          state.count += payload;
+        },
+      },
+      actions: {
+        decrement(context, payload) {
+          context.commit('decrement', payload);
+        },
+      },
+      getters: {
+        countX10: (state, getters) => {
+          return state.count * 10;
+        },
+      },
+    },
+  },
+})
