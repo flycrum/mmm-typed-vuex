4297869fdb77e0e1af3555c4614a8d88af067616
crummm <toddmz+github@gmail.com>
2018-06-16 00:12:35 -0500

    store module constructor now takes 2 params + accessing root store state has changed to be shorter

--- src/StoreModule.ts
+++ src/StoreModule.ts
@@ -1,26 +1,26 @@
 export class StoreModule {
   // STATIC PROPERTIES
-  public static rootStore: any;
+  public static rootStoreModule: StoreModule;
+  public static vuexStore: any;
 
   // PROPERTIES
   public moduleNamespace: string;
   public parentModule: StoreModule;
-  public store: any;
-  public state: any;
 
   // VARIABLES
   protected _modulePathCacheMap: {[path: string]: string};
 
   // CONSTRUCTOR
-  constructor() {
+  constructor(moduleNamespace: string, parentModule: StoreModule | boolean) {
+    this.moduleNamespace = moduleNamespace;
+    this.parentModule = typeof(parentModule) === 'boolean' ? undefined : parentModule;
     this._modulePathCacheMap = {};
   }
 
   // METHODS
-  public init(store: any) {
-    this.store = store;
-    this.state = store.state;
-    StoreModule.rootStore = store;
+  public init(vuexStore: any) {
+    StoreModule.vuexStore = vuexStore;
+    StoreModule.rootStoreModule = this;
   }
 
   public setOptions(options: any): void {
@@ -28,25 +28,20 @@ export class StoreModule {
   }
 
   public getModulePath(module: StoreModule, path?: string): string {
-    let cache: string = this._modulePathCacheMap[path];
-
-    if(cache) {
-      return cache;
-    }
-
-    return this._modulePathCacheMap[path] = this._processModulePath(module, path);
+    // use cached path OR determine path and cache that result
+    return this._modulePathCacheMap[path] || (this._modulePathCacheMap[path] = this._processModulePath(module, path));
   }
 
   public commit(mutationName: string, payload?: any, options?: any): any {
-    return StoreModule.rootStore.commit.call(StoreModule.rootStore, this.getModulePath(this, mutationName), payload, options);
+    return StoreModule.vuexStore.commit.call(StoreModule.vuexStore, this.getModulePath(this, mutationName), payload, options);
   }
 
   public dispatch(actionName: string, payload?: any, options?: any): any {
-    return StoreModule.rootStore.dispatch.call(StoreModule.rootStore, this.getModulePath(this, actionName), payload, options);
+    return StoreModule.vuexStore.dispatch.call(StoreModule.vuexStore, this.getModulePath(this, actionName), payload, options);
   }
 
   public get(getterName: string): any {
-    return StoreModule.rootStore.getters[this.getModulePath(this, getterName)];
+    return StoreModule.vuexStore.getters[this.getModulePath(this, getterName)];
   }
 
   // FUNCTIONS
